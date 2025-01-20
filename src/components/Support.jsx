9f8b57cbafe621c87fd9a899fd28d677e0fef573
Shashank Mahale <mahale.shashank04@gmail.com>
2025-01-20 12:43:51 +0530

    Update Support.jsx

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -22,16 +22,6 @@ const QuestionAnswer = () => {
       question: 'What is a component?',
       answer: 'A component is a reusable piece of code that manages its own state and renders UI.',
     },
-    {
-      id: 3,
-      question: 'Can SVR Robotics help with custom robotics solutions?',
-      answer: 'Yes, we specialize in both educational and application-based robotics solutions and can customize them to suit your needs.',
-    },
-    {
-      id: 4,
-      question:'Is SVR Robotics affiliated with any organizations?',
-      answer: 'We are the official techno-commercial partner for FEAST, developed by VSSC ISRO.',
-    }, 
     // Add more questions here
   ];
 
